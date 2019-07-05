3c3431ee70f744226949ca03f1db169ac84daefa
Hubert Salwa <hubesal@users.noreply.github.com>
2019-07-05 21:10:27 +0200

    fixed calculation

--- requests-rest.py
+++ requests-rest.py
@@ -1,17 +1,22 @@
 import requests
-
 respForm = {'format': 'json'}
 
-print('exchange rate PLN -> CHF')
-zl = input('type in money amount in PLN: ')
-requestFromNBP = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/CHF/today/', params = respForm)
+print('exchange rate PLN -> foreign currency')
+currencyCode = input('type in currency code: ')
+
+requestFromNBP = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + currencyCode + '/today/', params=respForm)
 
 print(requestFromNBP.status_code)
-response = requestFromNBP.json()
-print(response)
 
-foreignCurrency_lvl1 = response['rates'][0]
-foreignCurrency_lvl2 = foreignCurrency_lvl1['mid']
+if requestFromNBP.status_code == 404:
+    print('currency not found')
+else:
+    zl = input('type in money amount in PLN: ')
+    response = requestFromNBP.json()
+    print(response)
 
-amountExchange = round(float(foreignCurrency_lvl2)*float(zl),2)
-print(str(zl) + 'zl wynosi ' + str(amountExchange) + ' CHF')
+    foreignCurrency_lvl1 = response['rates'][0]
+    foreignCurrency_lvl2 = foreignCurrency_lvl1['mid']
+
+    amountExchange = round(float(float(zl)/foreignCurrency_lvl2), 2)
+    print(str(zl) + ' PLN is ' + str(amountExchange) + ' ' + currencyCode)
