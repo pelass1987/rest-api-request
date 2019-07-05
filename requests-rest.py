c7018bff2dc799854b2ad6025908e892177b1b9a
Hubert Salwa <hubesal@users.noreply.github.com>
2019-07-05 21:43:04 +0200

    added connection error exception handling

--- requests-rest.py
+++ requests-rest.py
@@ -1,22 +1,29 @@
 import requests
+
 respForm = {'format': 'json'}
 
 print('exchange rate PLN -> foreign currency')
 currencyCode = input('type in currency code: ')
 
-requestFromNBP = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + currencyCode + '/today/', params=respForm)
+try:
+    requestFromNBP = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + currencyCode + '/today/', params=respForm)
+    print(requestFromNBP.status_code)
 
-print(requestFromNBP.status_code)
+    if requestFromNBP.status_code == 404:
+        print('currency not found')
+    else:
+        zl = input('type in money amount in PLN: ')
+        response = requestFromNBP.json()
+        print(response)
 
-if requestFromNBP.status_code == 404:
-    print('currency not found')
-else:
-    zl = input('type in money amount in PLN: ')
-    response = requestFromNBP.json()
-    print(response)
+        foreignCurrency_lvl1 = response['rates'][0]
+        foreignCurrency_lvl2 = foreignCurrency_lvl1['mid']
 
-    foreignCurrency_lvl1 = response['rates'][0]
-    foreignCurrency_lvl2 = foreignCurrency_lvl1['mid']
+        amountExchange = round(float(float(zl)/foreignCurrency_lvl2), 2)
+        print(str(zl) + ' PLN is ' + str(amountExchange) + ' ' + currencyCode)
 
-    amountExchange = round(float(float(zl)/foreignCurrency_lvl2), 2)
-    print(str(zl) + ' PLN is ' + str(amountExchange) + ' ' + currencyCode)
+except requests.exceptions.ConnectionError as e:
+    print("connection error")
+    print(e)
+
+
